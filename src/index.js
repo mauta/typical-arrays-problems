8d80a11d1b31917013c4968faf18c4298df6b0a3
mauta <anna.opareva@gmail.com>
2020-09-27 17:22:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+   
+    if (array === undefined || array[0] === undefined ) {
+        return 0;
+    }
+    let result = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < result) {
+            result = array[i];
+        }
+    }
+    return result;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    
+    if (array === undefined || array[0] === undefined) {
+        return 0;
+    }
+    let result = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > result) {
+            result = array[i];
+        }
+    }
+    return result;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+    if (array === undefined || array[0] === undefined) {
+        return 0;
+    }
+    let result;
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum +=array[i]
+    }
+
+    result = sum /array.length;
+
+    return result;
 }
